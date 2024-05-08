0ab294e218679edc6c0ccef47ab46dc0791738f8
Abigael-lokilo <lokiloikala@gmail.com>
2024-05-08 16:59:57 +0100

    I changed my baner

--- src/components/baner.jsx
+++ src/components/baner.jsx
@@ -1,7 +1,25 @@
 function Baner() {
   return (
-    <div>
-      <p>Bon</p>
+    <div class="container mx-auto flex flex-row">
+      <h1 class="text-2xl font-bold text-center mb-4">
+        A propos de wedding planning by lkl
+      </h1>
+      <div class="flex-shrink-0">
+        <img
+          src="assets/pexels-arjunadinata-15157843.jpg"
+          alt="Image 1"
+          class="w-48 h-48 mr-4"
+        />
+      </div>
+      <div class="flex-grow">
+        <p class="text-center">
+          La planification d'un mariage est un processus passionnant et complexe
+          à la fois. Il implique l'organisation de tous les aspects de votre
+          célébration de mariage, de l'établissement d'un budget et de la liste
+          d'invités à la recherche du lieu, de la tenue et des prestataires
+          parfaits.
+        </p>
+      </div>
     </div>
   );
 }
